dd0f8c10f269c4a9479b5889ba9ae6182786d215
Zhenia Sigayev <sigayevzhenia@gmail.com>
2022-10-27 23:53:45 -0400

    Add HomeScreen

---
+++ src/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Banner from './Banner.js'
+import "./HomeScreen.css"
+import Nav from './Nav'
+
+
+function HomeScreen() {
+  return <div className="HomeScreen">
+    <Nav />
+
+
+    <Banner />
+    {/* Row */}
+
+
+
+
+
+  </div>
+}
+
+export default HomeScreen
